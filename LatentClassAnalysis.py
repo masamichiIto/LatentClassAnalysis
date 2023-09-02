41b396aa2a3c6e30860c8785abb869b366b9f7da
masamichiIto <masamichi.ito04@gmail.com>
2023-09-02 23:20:03 +0900

    lca script modified

--- LatentClassAnalysis.py
+++ LatentClassAnalysis.py
@@ -24,6 +24,9 @@ class LatentClassAnalysis:
                 ll2 += self.class_probs[k] * self._calc_bernoulli(data[i,:], k)
             ll += np.log(ll2)
         return ll
+    
+    def _calc_comp_ll(self, data, responsibilities):
+        
 
     
     def _calc_bernoulli(self, data_i, k):
@@ -103,79 +106,3 @@ print("Estimated Conditional Probabilities:")
 for k in range(n_classes):
     print(f"Class {k + 1}:", cond_probs[k])
 
-# ==================
-
-import numpy as np
-
-class LatentClassAnalysis:
-    def __init__(self, n_classes, n_iterations=100, tol=1e-4, random_state=None):
-        self.n_classes = n_classes
-        self.n_iterations = n_iterations
-        self.tol = tol
-        self.random_state = random_state
-        
-    def _e_step(self, data):
-        responsibilities = np.zeros((len(data), self.n_classes))
-        print(responsibilities.shape)
-        
-        for i in range(len(data)):
-            for k in range(self.n_classes):
-                responsibilities[i, k] = self.class_probs[k] * np.prod(self.cond_probs[k][data[i]])
-                
-            responsibilities[i, :] /= np.sum(responsibilities[i, :])
-        
-        return responsibilities
-    
-    def _m_step(self, data, responsibilities):
-        for k in range(self.n_classes):
-            self.class_probs[k] = np.mean(responsibilities[:, k])
-            
-            for j in range(self.n_categories):
-                self.cond_probs[k][j] = np.sum(responsibilities[:, k] * (data == j)) / np.sum(responsibilities[:, k])
-    
-    def fit(self, data):
-        self.n_samples, self.n_categories = data.shape
-        self.class_probs = np.ones(self.n_classes) / self.n_classes
-        self.cond_probs = np.random.rand(self.n_classes, self.n_categories)
-        
-        for iteration in range(self.n_iterations):
-            old_cond_probs = np.copy(self.cond_probs)
-            
-            responsibilities = self._e_step(data)
-            self._m_step(data, responsibilities)
-            
-            if np.max(np.abs(self.cond_probs - old_cond_probs)) < self.tol:
-                print(f"Converged after {iteration + 1} iterations.")
-                break
-    
-    def get_class_probabilities(self):
-        return self.class_probs
-    
-    def get_conditional_probabilities(self):
-        return self.cond_probs
-
-# Example usage
-data = np.array([[0, 1, 0, 1],
-                 [1, 0, 1, 0],
-                 [0, 1, 1, 0],
-                 [1, 0, 0, 1],
-                 [1, 1, 1, 0],
-                 [0, 0, 1, 1]])
-"""
-data = np.array([[0, 1, 0, 1],
-                 [1, 0, 1, 0],
-                 [0, 1, 1, 0],
-                 [1, 0, 0, 1]])
-"""
-
-n_classes = 2
-lca = LatentClassAnalysis(n_classes=n_classes)
-lca.fit(data)
-
-class_probs = lca.get_class_probabilities()
-cond_probs = lca.get_conditional_probabilities()
-
-print("Estimated Class Probabilities:", class_probs)
-print("Estimated Conditional Probabilities:")
-for k in range(n_classes):
-    print(f"Class {k + 1}:", cond_probs[k])
